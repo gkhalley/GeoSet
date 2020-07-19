3919ec7e97aae93356d12e212031003a80580a61
Glen Halley <Glen@gkhsoftware.com>
2020-07-19 08:06:21 -0500

    Dependencies
    Dependencies and unit testing. Mainly just to see new publish.

--- OutdoorWork.UnitTests/LineCrewSupervisorShould.vb
+++ OutdoorWork.UnitTests/LineCrewSupervisorShould.vb
@@ -1,31 +1,14 @@
 ﻿
 
+Imports AutoFixture.Xunit2
 Imports EngineeringOffice.SharedDatabase.Models
-Imports Ploeh.AutoFixture.Xunit2
 Imports Xunit
-
-
-Public Class CreateACrew
-
-End Class
-Public Class Calculator
-    Private _number1 As Double
-    Private _number2 As Double
-
-    Function Add(number1 As Double, number2 As Double) As Double
-        Add = number1 + number2
-        _number1 = number1
-        _number2 = number2
-    End Function
-End Class
 
 Public Class FieldNotesShould
     <Fact, AutoData>
     Public Sub OrderMaterial(number1 As Integer, number2 As Integer)
-        Dim sut = New Calculator()
-
-        Dim result = sut.Add(number1, number2)
-
+        Dim sut = New MaterialList()
+        Dim result = sut.Add(ItemNumber:=1, Quantity:=1)
         Assert.Equals(number1 + number2, result)
     End Sub
 
@@ -36,11 +19,23 @@ Public Class FieldNotesShould
 
     <Fact, InlineAutoData(42), InlineAutoData>
     Public Sub HaveACrew(number1 As Integer, number2 As Integer)
-        Dim sut = New Calculator()
+        Dim sut = New Crew()
 
-        Dim result = sut.Add(number1, number2)
+        Dim result = sut.Add(CrewName:="Glen's",CrewSize:=3)
 
         Assert.Equals(number1 + number2, result)
     End Sub
+End Class
+
+Public Class Crew
+    Public Function Add(CrewName As String, CrewSize As Integer) As Object
+        Throw New NotImplementedException
+    End Function
+End Class
+
+Public Class MaterialList
+    Public Function Add(ItemNumber As Integer, Quantity As Integer) As Object
+        Throw New NotImplementedException
+    End Function
 End Class
 
